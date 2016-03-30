a85e8b9bf84a85023d1d30b180893bf604d6d439
saragomezgarrigues <xarablaze@gmail.com>
2016-03-30 20:56:02 +0200

    Formulario Seguidor añadido. Empezando a usar regex. No se está usando hibernate.

---
+++ src/main/jav4/com/daw2/beans/Seguidor.java
@@ -0,0 +1,84 @@
+package com.daw2.beans;
+
+import java.io.Serializable;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@ViewScoped
+public class Seguidor implements Serializable {
+	//ATRIBUTOS DE LA CLASE SEGUIDOR
+	private static final long serialVersionUID = 1L;
+
+	private String nombre,apellidos,nickname,correo,password,rpassword;
+	
+	//CONSTRUCTOR DE LA CLASE
+	public Seguidor() {
+	
+	}
+	
+	//MÉTODOS DE LA CLASE 
+	public void registro(){
+		FacesMessage message = new FacesMessage("Bienvenido(a) " + nickname + "(" + nombre + " " + apellidos + ")",  
+				null);
+		FacesMessage m2=new FacesMessage("Correo electronico: " + correo);
+	    FacesContext.getCurrentInstance().addMessage(null, message);
+		FacesContext.getCurrentInstance().addMessage(null,m2);        
+	}
+	
+	
+	//GETTERS & SETTERS DE LOS ATRIBUTOS
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRpassword() {
+		return rpassword;
+	}
+
+	public void setRpassword(String rpassword) {
+		this.rpassword = rpassword;
+	}
+	
+
+	
+}
